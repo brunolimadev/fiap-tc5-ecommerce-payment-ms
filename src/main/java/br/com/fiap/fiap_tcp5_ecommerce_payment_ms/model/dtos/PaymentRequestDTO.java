599b9d0c43cadeee72b855bab3997bff3434b277
Eric Rangel <b.ericrangel@hotmail.com>
2024-07-13 23:09:33 -0300

    Feat: Creating DTOs

---
+++ src/main/java/br/com/fiap/fiap_tcp5_ecommerce_payment_ms/model/dtos/PaymentRequestDTO.java
@@ -0,0 +1,14 @@
+package br.com.fiap.fiap_tcp5_ecommerce_payment_ms.model.dtos;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+
+public record PaymentRequestDTO(
+
+        @NotBlank(message = "Informe o id do carrinho de compras")
+        String idShoppingCart,
+
+        @Valid
+        CardRequestDTO card,
+        double amount
+) { }
